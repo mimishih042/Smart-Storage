4ab670a857d253ddb21372bf3c948f01488c59b3
sabinachang <sabchang1@gmail.com>
2021-11-27 12:19:59 -0800

    Synchronize current and past list

--- shoppingList/current.js
+++ shoppingList/current.js
@@ -3,13 +3,13 @@ var list = [
     { name: 'Red Apple', qty:'10 lbs', expiration: '11/28/2021', id: 0},
     { name: 'Banana', qty:'3 lbs', expiration: '11/28/2021', id: 1 },
     { name: 'Salmon', qty:'3 lbs',expiration: '11/28/2021', id: 2 },
-    // { name: 'Pork', qty:'12 lbs', expiration: '12/06/2021', id: 3 },
-    // { name: 'Chicken Thigh', qty:'20 lbs',expiration: '12/24/2021', id: 4 },
+    { name: 'Pork', qty:'12 lbs', expiration: '12/06/2021', id: 3 },
+    { name: 'Chicken Thigh', qty:'20 lbs',expiration: '12/24/2021', id: 4 },
     // { name: 'Noodle', qty:'5 lbs', expiration: '12/15/2021', id: 5 },
     // { name: 'Lobster',qty:'10 lbs', expiration: '12/16/2021', id: 6 },
     // { name: 'Lamb', qty: '8 lbs', expiration: '01/15/2022', id: 7 },
     // { name: 'Green Tea Leaves', qty:'12 oz', expiration: '08/25/2022', id: 8 },
-    // { name: 'Lemon', qty:'10' ,expiration: '01/14/2022', id: 9 },
+    { name: 'Lemon', qty:'10' ,expiration: '01/14/2022', id: 9 },
     // { name: 'Milk', qty:'2', expiration: '12/11/2021', id: 10 },
 ];
 
@@ -18,44 +18,48 @@ var items = [];
 function decideBtnColor() {
     var checked =  document.querySelectorAll('input[type="checkbox"]:checked')
     for (var j = 0; j < checked.length; j++) {
-       var target = parseInt(checked[j].id)
-       var ele = list.find(({id}) => id === target)
+        var target = parseInt(checked[j].id)
+        var ele = list.find(({id}) => id === target)
+        
         items.push(ele)
+        list.splice(list.indexOf(ele), 1)
     }
     localStorage.setItem("purchased", JSON.stringify(items));
 }
-function firstItemMinus() {
-    var value = document.getElementById("item0").value
-    if (value > 0) {
-        document.getElementById("item0").value = value - 1
+
+function itemMinus(target) {
+    console.log("minus " + target)
+    var ele = list.find(({id}) => id === target)
+    var value = parseInt(ele.qty)
+    var unit = ele.qty.substr(ele.qty.indexOf(' ') + 1);
+
+    //Ignore unit if none 
+    if (unit == 10) {
+        unit = ""
     }
-}
-function secondItemMinus() {
-    var value = document.getElementById("item1").value
+    
     if (value > 0) {
-        document.getElementById("item1").value = value - 1
-    }
-}
-function thirdItemMinus() {
-    var value = document.getElementById("item2").value
-    if (value > 0) {
-        document.getElementById("item2").value = value - 1
+        ele.qty = (value - 1) + " " + unit
+        document.getElementById("item" + ele.id).value = value - 1
     }
 }
 
-function firstItemPlus() {
-    var value = parseInt(document.getElementById("item0").value) + 1
-    document.getElementById("item0").value = value
-}
-function secondItemPlus() {
-    var value = parseInt(document.getElementById("item1").value) + 1
-    document.getElementById("item1").value = value
-}
-function thirdItemPlus() {
-    var value = parseInt(document.getElementById("item2").value) + 1
-    document.getElementById("item2").value = value
-}
+function itemPlus(target) {
+    console.log("plus " + target)
 
+    var ele = list.find(({id}) => id === target)
+    var value = parseInt(ele.qty)
+    var unit = ele.qty.substr(ele.qty.indexOf(' ') + 1);
+
+    //Ignore unit if none 
+    if (unit == 10) {
+        unit = ""
+    }
+
+    console.log(unit)
+    ele.qty = (value + 1) + " " + unit
+    document.getElementById("item" + ele.id).value = value + 1  
+}
 
 function clearTable() {
     var table = document.getElementById('current-shopping-list')
@@ -69,20 +73,19 @@ function clearTable() {
 function renderList(data) {
     var table = document.getElementById('current-shopping-list')
     clearTable()
-    var indexes = ["first", "second", 'third']
     for (var i = 0; i < data.length; i++){
-        var ele = data[i];
+        const ele = data[i];
         var row = table.insertRow(i+1);
-        var index = indexes[i];
+        console.log(row)
         row.innerHTML = `
             <tr>
                 <td>${ele.name}</td>
                 <td>
                     <div class="mt-3">
                         <div class="input-counter">
-                        <span class="minus-btn" onclick="${index}ItemMinus()">-</span>
+                        <span class="minus-btn" >-</span>
                         <input id="item${ele.id}" type="text" value=${ele.qty}>
-                        <span class="plus-btn" onclick="${index}ItemPlus()">+</span>
+                        <span class="plus-btn" >+</span>
                         </div>
                     </div>
                 </td>
@@ -95,40 +98,13 @@ function renderList(data) {
                 </td>
             </tr>
         `
+        var minusBtn = row.getElementsByClassName('minus-btn')[0]
+        var plusBtn = row.getElementsByClassName("plus-btn")[0]
+
+        minusBtn.addEventListener('click', () => {itemMinus(ele.id)})
+        plusBtn.addEventListener('click',()=>{ itemPlus(ele.id)})
     }
 }
-
-
-
-document.addEventListener('mouseup', function (e) {
-    // var form1 = document.getElementById('changeDateForm1')
-    // var form2 = document.getElementById('changeDateForm2')
-    // var form3 = document.getElementById('changeDateForm3')
-    // var selected1 = document.getElementById("date1")
-    // var selected2 = document.getElementById("date2")
-    // var selected3 = document.getElementById("date3")
-    // if (!form1.contains(e.target)&&selected1.style.display === 'none'
-    // &&form1.style.display === 'block') {
-    //     var date1 = form1.newDateInput1.value
-    //     selected1.innerHTML = date1
-    //     selected1.style.display = 'block'
-    //     form1.style.display = 'none'
-    // }
-    // if (!form2.contains(e.target)&&selected2.style.display === 'none'
-    // &&form2.style.display === 'block') {
-    //     var date2 = form2.newDateInput2.value
-    //     selected2.innerHTML = date2
-    //     selected2.style.display = 'block'
-    //     form2.style.display = 'none'
-    // }
-    // if (!form3.contains(e.target)&&selected3.style.display === 'none'
-    // &&form3.style.display === 'block') {
-    //     var date3 = form3.newDateInput3.value
-    //     selected3.innerHTML = date3
-    //     selected3.style.display = 'block'
-    //     form3.style.display = 'none'
-    // }
-})
 
 function setTabLink() {
     const cur = document.getElementById("current");
@@ -141,6 +117,8 @@ function setTabLink() {
     })
 
     history.addEventListener('click', () => {
+        localStorage.setItem("currentList", JSON.stringify(list));
+
         history.className = "tablinks active";
         cur.className = "tablinks ";
         window.location.href = '../shoppingList/history.html'
@@ -156,10 +134,18 @@ function addToPushased() {
     }) 
 }
 
+function setList() {
+    var current = localStorage.getItem("currentList")
+
+    if (current) {
+        list = JSON.parse(current)
+    }
+}
+
 function main() {
     setTabLink();
+    setList()
     renderList(list);
     addToPushased();
-
 }
 document.addEventListener('DOMContentLoaded', main);
